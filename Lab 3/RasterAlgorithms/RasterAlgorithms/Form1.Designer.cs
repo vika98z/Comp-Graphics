078333e96d362135896a40b98b9a7e585b0f8449
vika98z <vika98z@mail.ru>
2019-09-23 12:35:53 +0300

    lab 3.1

---
+++ Lab 3/RasterAlgorithms/RasterAlgorithms/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace RasterAlgorithms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1 = new System.Windows.Forms.PictureBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.panel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Цвет заливки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Image = ((System.Drawing.Image)(resources.GetObject("panel1.Image")));
+            this.panel1.Location = new System.Drawing.Point(130, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(658, 426);
+            this.panel1.TabIndex = 2;
+            this.panel1.TabStop = false;
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseClick);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Panel1_MouseUp);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 102);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 56);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Выбрать точку для заливки";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 38);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Заливать картинкой";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(794, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(658, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.panel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Lab 3/RasterAlgorithms/RasterAlgorithms/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RasterAlgorithms
+{
+    public partial class Form1 : Form
+    {
+        Color BorderColor = Color.Black;
+        Color FillColor = Color.Green;
+        bool isPressed = false;
+        Point CurrentPoint;
+        Point PrevPoint;
+        Graphics g;
+
+        bool isFilling = false;
+
+        Bitmap DrawArea;
+        Bitmap PictureFillArea;
+
+        bool isPictureFilling;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //PictureFillArea = Bitmap.FromFile("d:\\Users\\stety\\Desktop\\fruits.jpg") as Bitmap;//f.picBitmap;
+            //new Bitmap(panel1.Size.Width, panel1.Size.Height);
+            PictureFillArea = (Bitmap)pictureBox1.Image;
+
+            panel1.Image = null;
+            panel1.Refresh();
+
+            DrawArea = new Bitmap(panel1.Size.Width, panel1.Size.Height);
+            panel1.Image = DrawArea;
+            g = Graphics.FromImage(DrawArea);
+            SetBorders();
+
+            isPictureFilling = false;
+        }
+
+        private void SetBorders()
+        {
+            Pen blackPen = new Pen(BorderColor, 1);
+            Rectangle rect = new Rectangle(1, 1, panel1.Width - 3, panel1.Height - 3);
+            g.DrawRectangle(blackPen, rect);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DialogResult D = colorDialog1.ShowDialog();
+            if (D == DialogResult.OK)
+                FillColor = colorDialog1.Color;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            panel1.Image = null;
+            panel1.Refresh();
+
+            DrawArea = new Bitmap(panel1.Size.Width, panel1.Size.Height);
+            panel1.Image = DrawArea;
+            g = Graphics.FromImage(DrawArea);
+
+            SetBorders();
+
+            isFilling = false;
+            button5.Text = "Выбрать точку для заливки";
+
+            isPictureFilling = false;
+            button3.Text = "Заливка изображением";
+        }
+
+        private void Panel1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isFilling)
+            {
+                fill(Convert.ToInt32(e.X), Convert.ToInt32(e.Y));
+                panel1.Image = DrawArea;
+            }
+        }
+
+        private void Panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (!isFilling)
+            {
+                isPressed = true;
+                CurrentPoint = e.Location;
+            }
+            
+        }
+
+        private void Panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isFilling)
+            {
+                if (isPressed)
+                {
+                    PrevPoint = CurrentPoint;
+                    CurrentPoint = e.Location;
+                    paint_simple();
+                }
+            }
+        }
+
+        private void paint_simple()
+        {
+            Pen p = new Pen(BorderColor, 4);
+            g.DrawLine(p, PrevPoint, CurrentPoint);
+            panel1.Image = DrawArea;
+        }
+
+        private void Panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isPressed = false;
+        }
+
+        private int lineFill(int x, int y, int dir, int prevXl, int prevXr)
+        {
+            int xl = x;
+            int xr = x;
+            Color c;
+
+            if (!isPictureFilling)
+            {
+                do
+                {
+                    c = DrawArea.GetPixel(--xl, y);
+                } while ((c.ToArgb() != BorderColor.ToArgb()) && (c.ToArgb() != FillColor.ToArgb()) && (xl > 0));
+            }
+            else
+            {
+                do
+                {
+                    c = DrawArea.GetPixel(--xl, y);
+                } while ((c.ToArgb() != BorderColor.ToArgb()) && (c.ToArgb() != PictureFillArea.GetPixel(xl, y).ToArgb()) && (xl > 0));
+            }
+
+            if (!isPictureFilling)
+            {
+                do
+                {
+                    c = DrawArea.GetPixel(++xr, y);
+                } while ((c.ToArgb() != BorderColor.ToArgb()) && (c.ToArgb() != FillColor.ToArgb()) && (xr < panel1.Width - 1));
+            }
+            else
+            {
+                do
+                {
+                    c = DrawArea.GetPixel(++xr, y);
+                } while ((c.ToArgb() != BorderColor.ToArgb()) && (c.ToArgb() != PictureFillArea.GetPixel(xr, y).ToArgb()) && (xr < panel1.Width - 1));
+            }
+
+            xl++;
+            xr--;
+
+            if (!isPictureFilling)
+            {
+                Pen p = new Pen(FillColor);
+                g.DrawLine(p, xl, y, xr, y);
+            }
+            else
+            {
+                for (int i = xl; i <= xr; i++)
+                {
+                    DrawArea.SetPixel(i, y, PictureFillArea.GetPixel(i, y));
+                }
+            }
+
+            for (x = xl; x <= xr; x++)
+            {
+                c = DrawArea.GetPixel(x, y + dir);
+                if ((c.ToArgb() != BorderColor.ToArgb()) && (c.ToArgb() != FillColor.ToArgb()) && (y + dir < panel1.Height - 1))
+                {
+                    x = lineFill(x, y + dir, dir, xl, xr);
+                }
+            }
+
+            for (x = xl; x < prevXl; x++)
+            {
+                c = DrawArea.GetPixel(x, y - dir);
+                if ((c.ToArgb() != BorderColor.ToArgb()) && (c.ToArgb() != FillColor.ToArgb()) && (y - dir > 0))
+                {
+                    x = lineFill(x, y - dir, -dir, xl, xr);
+                }
+            }
+
+            for (x = prevXr; x < xr; x++)
+            {
+                c = DrawArea.GetPixel(x, y - dir);
+                if ((c.ToArgb() != BorderColor.ToArgb()) && (c.ToArgb() != FillColor.ToArgb()) && (y - dir > 0))
+                {
+                    x = lineFill(x, y - dir, -dir, xl, xr);
+                }
+            }
+
+            return xr;
+        }
+
+        private void fill(int x, int y)
+        {
+            lineFill(x, y, 1, x, x);
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            isFilling = !isFilling;
+            if (isFilling)
+                button5.Text = "Рисовать далее";
+            else
+                button5.Text = "Выбрать точку для заливки";
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            isPictureFilling = !isPictureFilling;
+            if (isPictureFilling)
+                button3.Text = "Заливка цветом";
+            else
+                button3.Text = "Заливка изображением";
+        }
+    }
+}
