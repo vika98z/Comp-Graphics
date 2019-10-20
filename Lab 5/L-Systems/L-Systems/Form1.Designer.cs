d961530d827b475fabe6733b8dd8ab0b7b8a5cfd
vika98z <vika98z@mail.ru>
2019-10-20 23:24:20 +0300

    L-SYSTEMS

--- Lab 5/L-Systems/L-Systems/Form1.Designer.cs
+++ Lab 5/L-Systems/L-Systems/Form1.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(550, 550);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(581, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(581, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Построить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(703, 56);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(80, 20);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(581, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Количество итераций";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(581, 158);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Очистить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(793, 571);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Lab 5/L-Systems/L-Systems/Form1.cs
+++ Lab 5/L-Systems/L-Systems/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,150 @@ namespace L_Systems
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        string axiom;
+        double angle;
+
+        string filename;
+        SortedDictionary<char, string> rules;
+
+        Stack<Tuple<double, double, double, double>> states;
+        int iterations;
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+            rules = new SortedDictionary<char, string>();
+
+            states = new Stack<Tuple<double, double, double, double>>();
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rules.Clear();
+            string[] rule;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    filename = openFileDialog1.FileName;
+                    string[] lines = File.ReadAllLines(filename);
+                    string[] parameters = lines[0].Split(' ');
+                    axiom = parameters[0];
+                    angle = Convert.ToDouble(parameters[1]);                   
+                    for (int i = 1; i < lines.Length; ++i)
+                    {
+                        rule = lines[i].Split('=');
+                        rules[Convert.ToChar(rule[0])] = rule[1];
+                    }
+                    button2.Enabled = true;
+                }
+                catch
+                {
+                    button2.Enabled = false;
+                    DialogResult result = MessageBox.Show("Ошибка открытия файла",
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+
+            iterations = (int)numericUpDown1.Value;
+            List<double> xPoints = new List<double>();
+            List<double> yPoints = new List<double>();
+            List<Tuple<double, double, double, double>> lSystPoints =
+                new List<Tuple<double, double, double, double>>();
+            double x = pictureBox1.Width, y = pictureBox1.Height / 2, dy = 0,
+                dx = -(pictureBox1.Width / Math.Pow(10, iterations + 1));
+            
+            if (filename.Contains("Куст"))
+            {
+                x = pictureBox1.Width / 2;
+                y = 0;
+                dy = pictureBox1.Height / Math.Pow(10, iterations + 1);
+                dx = 0;
+            }
+
+            xPoints.Add(x);
+            yPoints.Add(y);
+
+            string prev = axiom;
+            string next = axiom;
+            int iter = 0;
+            while (iter++ < iterations)
+            {
+                prev = next;
+                next = "";
+                for (int i = 0; i < prev.Length; ++i)
+                {
+                    if (rules.ContainsKey(prev[i]))
+                        next += rules[prev[i]];
+                    else
+                        next += prev[i];
+                }
+            }
+            
+            double rx, ry;
+            for (int i = 0; i < next.Length; ++i)
+            {
+                switch (next[i])
+                {
+                    case 'F':
+                        lSystPoints.Add(
+                            new Tuple<double, double, double, double>(x, y, x + dx, y + dy));
+                        x += dx;
+                        y += dy;
+                        xPoints.Add(x);
+                        yPoints.Add(y);
+                        break;
+                    case '+':
+                        rx = dx;
+                        ry = dy;
+                        dx = rx * Math.Cos(angle * Math.PI / 180) - ry * Math.Sin(angle * Math.PI / 180);
+                        dy = rx * Math.Sin(angle * Math.PI / 180) + ry * Math.Cos(angle * Math.PI / 180);
+                        break;
+
+                    case '-':
+                        rx = dx;
+                        ry = dy;
+                        dx = rx * Math.Cos(-angle * Math.PI / 180) - ry * Math.Sin(-angle * Math.PI / 180);
+                        dy = rx * Math.Sin(-angle * Math.PI / 180) + ry * Math.Cos(-angle * Math.PI / 180);
+                        break;
+                    case '[':
+                        states.Push(new Tuple<double, double, double, double>(x, y, dx, dy));
+                        break;
+
+                    case ']':
+                        Tuple<double, double, double, double> coords = states.Pop();
+                        x = coords.Item1;
+                        y = coords.Item2;
+                        dx = coords.Item3;
+                        dy = coords.Item4;
+                        break;
+                    default: break;
+                }
+            }
+
+            double scale = Math.Max(xPoints.Max() - xPoints.Min(), yPoints.Max() - yPoints.Min());
+
+            foreach (var p in lSystPoints)
+                g.DrawLine(Pens.Blue, (float)((xPoints.Max() - p.Item1) / scale * pictureBox1.Width),
+                    (float)((yPoints.Max() - p.Item2) / scale * pictureBox1.Height), (float)((xPoints.Max() - p.Item3) / scale * pictureBox1.Width),
+                    (float)((yPoints.Max() - p.Item4) / scale * pictureBox1.Height));
+
+            pictureBox1.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Invalidate();
         }
     }
 }
