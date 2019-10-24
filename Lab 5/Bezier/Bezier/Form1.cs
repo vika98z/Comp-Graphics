fdb029e5b39b1bbf4b0fd62e00d309bbcfd5e227
vika98z <vika98z@mail.ru>
2019-10-24 21:06:50 +0300

    bazier

---
+++ Lab 5/Bezier/Bezier/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bezier
+{
+    public partial class Form1 : Form
+    {
+        private Graphics g;
+        private List<PointF> points = new List<PointF>();
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+        }
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            points.Add(e.Location);
+            g.FillEllipse(new SolidBrush(Color.Red), e.X - 2.5f, e.Y - 2.5f, 5, 5);
+            pictureBox1.Invalidate();
+        }
+
+        private int Fuctorial(int n)
+        {
+            int res = 1;
+            for (int i = 1; i <= n; i++)
+                res *= i;
+            return res;
+        }
+
+        // Функция вычисления полинома Бернштейна
+        float polinom(int i, int n, float t)
+        {
+            return ((float)Fuctorial(n) / (Fuctorial(i) * Fuctorial(n - i))) * (float)Math.Pow(t, i) * (float)Math.Pow(1 - t, n - i);
+        }
+
+        void thirdBezier(List<PointF> pointFs)
+        {
+            int j = 0;
+            float step = 0.01f;
+
+            PointF[] result = new PointF[101];
+            for (float t = 0; t <= 1; t += step)
+            {
+                float ytmp = 0;
+                float xtmp = 0;
+                for (int i = 0; i < pointFs.Count; i++)
+                {
+                    float b = polinom(i, pointFs.Count - 1, t);
+                    xtmp += pointFs[i].X * b;
+                    ytmp += pointFs[i].Y * b;
+                }
+                result[j] = new PointF(xtmp, ytmp);
+                j++;
+            }
+            g.DrawLines(new Pen(Color.Blue), result);
+            pictureBox1.Invalidate();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var b = SplitPoints(points, 3);
+            for (int i = 0; i < b.Count(); i++)
+            {
+                List<PointF> tmp = new List<PointF>();
+                foreach (var v in b.ElementAt(i))
+                    tmp.Add(v);
+                if (i + 1 < b.Count())
+                    tmp.Add(b.ElementAt(i + 1).ElementAt(0));
+                thirdBezier(tmp);
+            }
+        }
+
+        private IEnumerable<IEnumerable<PointF>> SplitPoints(IEnumerable<PointF> source, int count)
+        {
+            return source.Select((x, y) => new { Index = y, Value = x })
+              .GroupBy(x => x.Index / count)
+              .Select(x => x.Select(y => y.Value).ToList())
+              .ToList();
+        }
+    }
+}
---
+++ Lab 5/Bezier/Bezier/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Bezier")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bezier")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("7f1bdf34-b63c-4696-b07d-545614bb0b1f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
